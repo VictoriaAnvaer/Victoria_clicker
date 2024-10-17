fcc31dc16f85303a9de6ac1c9cde688dedac9949
VictoriaAnvaer <145706598+VictoriaAnvaer@users.noreply.github.com>
2024-10-17 15:16:10 -0400

    Add files via upload
    final

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // U2L3 Build a Counter App
 var count = 0;
-var exit = 0;
 var countHTML = document.getElementById("count-el");
 document.getElementById("count-el").innerText = count;
 var saved = document.getElementById("save-p");
@@ -12,18 +11,6 @@ function increment() {
     console.log(countHTML);
 }
 
-function decrement() {
-    if (count > 0) {
-        count--;
-    countHTML.innerText = count;
-    }
-}
-
 function save() {
     saved.innerText = saved.innerText + " " + count + " -";
-}
-
-function reset() {
-    count = 0;
-    countHTML.innerText = count;
 }
